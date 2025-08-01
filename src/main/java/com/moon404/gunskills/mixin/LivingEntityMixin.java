02e28b23d74caa20143c24f8d0766c5ea2339597
Harrison <gamerulekeep1234@gmail.com>
2025-08-01 19:06:38 +0800

    worldborder可以自由进出，不会在worldborder静止时玩家出不去，并且玩家可以与worldborder外面的物品交互,添加了相关注释

--- src/main/java/com/moon404/gunskills/mixin/LivingEntityMixin.java
+++ src/main/java/com/moon404/gunskills/mixin/LivingEntityMixin.java
@@ -1,13 +1,10 @@
 package com.moon404.gunskills.mixin;
 
-import net.minecraft.world.level.border.WorldBorder;
-import net.minecraft.world.phys.AABB;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import net.minecraft.world.entity.LivingEntity;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin
@@ -16,18 +13,5 @@ public class LivingEntityMixin
     private double changeWorldBorderDamage(double d0)
     {
         return -1;
-    }
-
-
-    @Redirect(
-            method = {"baseTick"},
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/world/level/border/WorldBorder;isWithinBounds(Lnet/minecraft/world/phys/AABB;)Z"
-            )
-    )
-    public boolean mixin_isWithinBounds(WorldBorder wb, AABB bb) {
-        return bb.minX > wb.getMinX() && bb.maxX < wb.getMaxX()
-                && bb.minZ > wb.getMinZ() && bb.maxZ < wb.getMaxZ();
     }
 }
--- src/main/java/com/moon404/gunskills/mixin/WorldBorderMixin.java
+++ src/main/java/com/moon404/gunskills/mixin/WorldBorderMixin.java
@@ -6,63 +6,49 @@ import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.border.WorldBorder;
 import net.minecraft.world.phys.AABB;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.Overwrite;
 
 @Mixin(WorldBorder.class)
 public class WorldBorderMixin {
-    @Inject(
-            method = {"isWithinBounds(DD)Z"},
-            at = {@At("HEAD")},
-            cancellable = true
-    )
-    public void mixin_isWithinBounds_0(double p_156094_, double p_156095_, CallbackInfoReturnable<Boolean> cir) {
-        cir.setReturnValue(true);
+
+
+    //所有判断世界实体处于世界边界内的方法全返回true
+
+    @Overwrite
+    public boolean isWithinBounds(BlockPos pPos)
+    {
+        return true;
     }
 
-    @Inject(
-            method = {"isWithinBounds(DDD)Z"},
-            at = {@At("HEAD")},
-            cancellable = true
-    )
-    public void mixin_isWithinBounds_1(double p_187563_, double p_187564_, double p_187565_, CallbackInfoReturnable<Boolean> cir) {
-        cir.setReturnValue(true);
+    @Overwrite
+    public boolean isWithinBounds(ChunkPos pChunkPos)
+    {
+        return true;
     }
 
-    @Inject(
-            method = {"isWithinBounds(Lnet/minecraft/world/phys/AABB;)Z"},
-            at = {@At("HEAD")},
-            cancellable = true
-    )
-    public void mixin_isWithinBounds_2(AABB p_61936_, CallbackInfoReturnable<Boolean> cir) {
-        cir.setReturnValue(true);
+    @Overwrite
+    public boolean isWithinBounds(double pX, double pZ)
+    {
+        return true;
     }
 
-    @Inject(
-            method = {"isWithinBounds(Lnet/minecraft/core/BlockPos;)Z"},
-            at = {@At("HEAD")},
-            cancellable = true
-    )
-    public void mixin_isWithinBounds_3(BlockPos p_61938_, CallbackInfoReturnable<Boolean> cir) {
-        cir.setReturnValue(true);
+    @Overwrite
+    public boolean isWithinBounds(double pX, double pZ, double pOffset)
+    {
+        return true;
     }
 
-    @Inject(
-            method = {"isWithinBounds(Lnet/minecraft/world/level/ChunkPos;)Z"},
-            at = {@At("HEAD")},
-            cancellable = true
-    )
-    public void mixin_isWithinBounds_4(ChunkPos p_61928_, CallbackInfoReturnable<Boolean> cir) {
-        cir.setReturnValue(true);
+    @Overwrite
+    public boolean isWithinBounds(AABB pBox)
+    {
+        return true;
     }
 
-    @Inject(
-            method = {"isInsideCloseToBorder"},
-            at = {@At("HEAD")},
-            cancellable = true
-    )
-    public void mixin_isInsideCloseToBorder(Entity p_187567_, AABB p_187568_, CallbackInfoReturnable<Boolean> cir) {
-        cir.setReturnValue(false);
+
+    //实体在边界时不再被认为有碰撞
+    @Overwrite
+    public boolean isInsideCloseToBorder(Entity pEntity, AABB pBounds)
+    {
+        return false;
     }
 }
